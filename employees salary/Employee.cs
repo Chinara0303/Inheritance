9f0b1d3cbc2b83450c763e3e5232a8fc90a7a65a
[Chinara0303] <chinaraei@code.edu.az>
2021-12-03 01:55:56 +0400

    ...

---
+++ employees salary/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace employees_salary
+{
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public string Positon;
+        public int DailySalary;
+
+        //public Employee(string name, string surname, string positon, int dailysalary)
+        //{
+        //    Name = name;
+        //    Surname = surname;
+        //    Positon = positon;
+        //    DailySalary = dailysalary;
+        //}
+
+        public void MonthlySalary (string Name, string Surname, string Positon, int DailySalary)
+        {
+            int MonthlySalary = DailySalary * 30;
+            Console.WriteLine(MonthlySalary);
+
+
+        }
+    }
+}
---
+++ employees salary/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace employees_salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("Chinara","Ibadova","Backend developer",80);
+            //Employee emp = new Employee();
+            //emp.Name = "Chinara";
+            //emp.Surname = "Ibadova";
+            //emp.Positon = "Backend developer";
+            //emp.DailySalary = 80;
+            emp.MonthlySalary(emp.Name, emp.Surname, emp.Positon, emp.DailySalary);
+
+        }
+    }
+}
